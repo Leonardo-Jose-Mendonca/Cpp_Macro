09647b46119aa19e014640357a3a06436527aa9d
Leonardo José Mendonça <ljmendonca@hotmail.com>
2022-11-15 10:00:32 -0300

    Tested Debug and changed macro condition

--- Macro/Macro.cpp
+++ Macro/Macro.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 #define MAIN int main(){
 #define CLOSE_MAIN }
-#ifdef _PRINTER_BIT
+#ifndef _PRINTER_BIT
 #define PRINT_BITS(byte) "%d%d%d%d%d%d%d%d",\
 		((byte >> 7) & 0x01), ((byte >> 6) & 0x01), ((byte >> 5) & 0x01), ((byte >> 4) & 0x01), \
 		((byte >> 3) & 0x01), ((byte >> 2) & 0x01), ((byte >> 1) & 0x01), ((byte >> 0) & 0x01)
-#else
-#define PRINT_BITS(byte) "%d", byte
 #endif
 
 MAIN
-int x = 170;				// decimal value
+int x = 0xA0B1C2D3;				// decimal value
 printf(PRINT_BITS(x));		// print the bits of the variable 'x' if the _PRINTER_BIT is defined
 CLOSE_MAIN
